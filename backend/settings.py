786b3988366de96bf522cfae881fbe9fe9cfcb1b
chiscocode <patrickchukwudifu716@gmail.com>
2022-10-17 16:05:19 +0100

    make it better

--- backend/settings.py
+++ backend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-4le7+fn!d^ndym1tj5ou!35&1k(tca6&jx-*7v&-(kvee1cy$-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 
 # Application definition
